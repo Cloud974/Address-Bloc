58320bab0a366b654a913d1cb4c2d2baa14f8e73
Michael Fournier <4niermike@gmail.com>
2017-08-22 21:21:32 -0400

    Add remove entry method

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe AddressBook do
       expect(book.entries.size).to eq(1)
     end
 
-    it "adds teh correct information to entries" do
+    it "adds the correct information to entries" do
       book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       new_entry = book.entries[0]
@@ -34,6 +34,23 @@ RSpec.describe AddressBook do
       expect(new_entry.name).to eq('Ada Lovelace')
       expect(new_entry.phone_number).to eq('010.012.1815')
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
+  end
+
+  describe "#remove_entry" do
+    it "removes entry using name, phone_number, and email" do
+      book = AddressBook.new
+      book.add_entry('Michael Faraday', '011.012.1815', 'm@faraday.com')
+
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Michael Faraday')
     end
   end
 
